5c25124937e1048cec42d9b764d5bc9dbe3e360d
Karan Pal <karan@digiscribe.in>
2021-02-02 17:56:04 +0530

    Initial Commit

---
+++ Non-Interactive Transition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Non-Interactive Transition
+//
+//  Created by Karan Pal on 02/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
